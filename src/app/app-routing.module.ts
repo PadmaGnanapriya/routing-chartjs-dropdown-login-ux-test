b95f4ce5fef78bd03792aa7ab7599e792bc680a8
PadmaGnanapriya <padmaisuru@gmail.com>
2020-12-16 07:28:14 +0530

    Signin signup ux is created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SigninSignupComponent} from './signin-signup/signin-signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SigninSignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
